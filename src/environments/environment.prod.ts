ae1c16a6b128e0161d00750739084404c781cb99
armin <armintbn@hotmail.com>
2020-08-30 21:58:46 +0430

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,3 +2,10 @@ export const environment = {
   production: true
 };
 
+export const marvel = {
+  url: 'https://gateway.marvel.com/v1/public/characters?',
+  ts: '1565922410',
+  apikey:'6a038473ffd6407750a2ea27115f7e7c',
+  hash:'1492df65a88ef98a1a279719fe509f72'
+}
+
